650e4c791196b22bc43faa23bc4911a92d6f4f86
karimgaberhd12 <karimgabernew@gmail.com>
2025-08-31 08:06:27 +0300

    first commit

---
+++ src/components/NoteForm.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import TextInput from "./inputs/TextInput";
+import SelectInput from "./inputs/SelectInput";
+import TextAreaInput from "./inputs/TextAreaInput";
+const NoteForm = ({ notes, setNotes }) => {
+  const [formData, setFormData] = useState({
+    title: "",
+    category: "Work",
+    priority: "Medium",
+    description: "",
+  });
+
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // validation
+    if (!formData.title || !formData.description) return;
+
+    // create note object
+    const newNote = {
+      id: Date.now(),
+      ...formData,
+    };
+    // add notes to state
+    setNotes([newNote, ...notes]);
+    // reset form date
+    setFormData({
+      title: "",
+      category: "Work",
+      priority: "Medium",
+      description: "",
+    });
+  };
+  return (
+    <>
+      <button
+        onClick={() => setIsFormVisible(!isFormVisible)}
+        className="w-full bg-gray-100 border border-gray-300 text-purple-800 py-2 rouded-lg cursor-pointer hover:bg-purple-200 hover:border-purple-300 transition mb-4"
+      >
+        {isFormVisible ? "Hide Form ✖️" : "Add New Note ➕"}
+      </button>
+
+      {isFormVisible && (
+        <form onSubmit={handleSubmit} className="mb-6">
+          <TextInput
+            label="Title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+          />
+
+          <SelectInput
+            label="Priority"
+            name="priority"
+            value={formData.priority}
+            onChange={handleChange}
+            options={[
+              { value: "High", label: "🔴High" },
+              { value: "Medium", label: "🟡Medium" },
+              { value: "Low", label: "🔵Low" },
+            ]}
+          />
+
+          <SelectInput
+            label="Category"
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+            options={[
+              { value: "Work", label: "📂Work" },
+              { value: "Personal", label: "🏡Personal" },
+              { value: "Ideas", label: "💡Ideas" },
+            ]}
+          />
+          <TextAreaInput
+            label="Description"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
+          <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600">
+            Add Note
+          </button>
+        </form>
+      )}
+    </>
+  );
+};
+
+export default NoteForm;
